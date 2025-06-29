11d69067a9fbea8171b60e9ff1181ab5c8e757a5
AbdielMamani <abdielmamani@upeu.edu.pe>
2025-06-29 17:34:46 -0500

    last change

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $programas = $mysqli->query("SELECT id, programa FROM t_programa");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Encuesta docente</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <header>
@@ -733,13 +734,7 @@ $programas = $mysqli->query("SELECT id, programa FROM t_programa");
                             </div>
                         </div>
 
-
-
-
-
-
-
-                        <strong><i>Puntaje Global: </i><strong><input id="scor" type="text" name="suma"  />        
+                        <input   id="scor" type="text" name="suma"/>        
 
                 <div class="card-footer bg-transparent ">
                     <div class="input-group row">
